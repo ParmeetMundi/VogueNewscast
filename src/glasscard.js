7c6419421c8260c617fd2359720b5866d19577d7
samardeol <samardeol@gmail.com>
2021-09-27 14:29:16 +0530

    main page

---
+++ src/glasscard.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import profile from './images/back.jpg';
+import styled from 'styled-components';
+import { useSpring, animated, config } from 'react-spring';
+
+const Container = styled(animated.div)`
+display: inline-block;
+padding: 3em;
+background: #C7D2FE66;
+border-radius: 10px;
+z-index: 1;
+position: relative;
+backdrop-filter: blur(10px);
+border: 2px solid transparent;
+background-clip: border-box;
+cursor: pointer;
+margin: 2em;
+`;
+
+/*const StyledImg = styled.img`
+    width: 200px;
+    height: auto;
+    border: 2px solid #000;
+    border-radius: 50%;
+`;
+<StyledImg src={profile} />
+FOR IMAGE */
+
+const StyledH1 = styled.h1`
+    line-heright: 1.5;
+    letter-spacing: 1.5;
+    font-family: "Gilroy";
+`;
+
+const StyledH3 = styled.h3`
+    line-heright: 1.5;
+    letter-spacing: 1.15;
+    font-family: "Gilroy";
+    font-size: 20px;
+`;
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const GlassCardTwitter = () => {
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1] , config: config.default}))
+    return (
+        <Container
+            onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
+            onMouseLeave={() => set({xys:[0,0,1]})}
+            style={{
+                transform: props.xys.interpolate(trans)
+            }}
+        >
+            
+            <StyledH1>Twitter</StyledH1>
+            <StyledH3>Tweet<br/> Likes and Retweets </StyledH3>
+        </Container>
+    );
+}
+
+
+export default GlassCardTwitter;
